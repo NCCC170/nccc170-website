e25733c58ed5903b1c601aad325cc4094cfb99eb
Aaron Rendahl <rend0020@umn.edu>
2023-09-11 17:47:14 -0500

    code to create all the generated markdown files

---
+++ R/_build_site.R
@@ -0,0 +1,15 @@
+## if running the server, best to stop before rebuilding the site
+blogdown::stop_server()
+
+## download info from the google drive
+source(here::here("R", "download_info.R"))
+source(here::here("R", "download_photos.R"))
+
+## and rebuild the automatically generated parts of the site
+source(here::here("R", "build_people.R"))
+source(here::here("R", "build_meetings.R"))
+source(here::here("R", "build_publications.R"))
+
+rmarkdown::render_site(encoding = 'UTF-8')
+
+blogdown::serve_site()
